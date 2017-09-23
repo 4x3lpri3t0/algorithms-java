32bb28c5c916e1dc3ab92c5b91f112d5b0a7f6b4
Axel Prieto <prietoaxel@gmail.com>
2017-09-23 20:51:39 +0200

    Initial commit

---
+++ src/algorithms/java/Main.java
@@ -0,0 +1,10 @@
+package algorithms.java;
+
+import algorithms.java.hr.implementation.FlatlandSpaceStations;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FlatlandSpaceStations.main(args);
+    }
+}
