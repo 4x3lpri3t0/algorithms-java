46071fbdb799493e7742976a47b891f8dbbc9cca
Axel Prieto <prietoaxel@gmail.com>
2018-03-09 09:24:03 +0100

    minor changes

---
+++ LeetCode/src/algorithms/Main.java
@@ -0,0 +1,6 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print(_771_Jewels_and_Stones.numJewelsInStones("aA", "aAAbbbb"));
+    }
+}
---
+++ LeetCode/src/algorithms/java/_771_Jewels_and_Stones.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class _771_Jewels_and_Stones {
+    public static int numJewelsInStones(String J, String S) {
+        int total = 0;
+        for (int i = 0; i < S.length(); i++) {
+            Character current = S.charAt(i);
+
+            if (J.indexOf(current) != -1)
+                total++;
+        }
+
+        return total;
+    }
+}
