89550474e89a64664f87484eb701adb077db742f
Axel Prieto <prietoaxel@gmail.com>
2018-07-15 12:02:44 +0200

    minor changes

---
+++ HackerRank/src/_30daysofcode/_25_RunningTime.java
@@ -0,0 +1,20 @@
+package _30daysofcode;
+
+import java.util.Scanner;
+
+import static java.lang.System.console;
+import static java.lang.System.out;
+
+public class _25_RunningTime {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        int n = in.nextInt();
+
+        while (T-- > 0) {
+
+
+            out.println();
+        }
+    }
+}
