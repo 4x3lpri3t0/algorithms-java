98e183e1ee17ea5046ce5253a0083814adf8d1bd
Axel Prieto <prietoaxel@gmail.com>
2018-07-18 09:43:42 +0200

    Daily programming practice

--- CtCI/src/_2_3_DeleteMiddleNode.java
+++ CtCI/src/_2_3_DeleteMiddleNode.java
@@ -1,27 +1,28 @@
-import java.io.*;
-import java.util.*;
+import static java.lang.System.out;
 
 public class _2_3_DeleteMiddleNode {
-    private static LLNode delete(LLNode nodeToDelete) {
-        if (nodeToDelete.next == null) {
-            return null; // Invalid
+    private static boolean tryDelete(LLNode nodeToDelete) {
+        if (nodeToDelete == null || nodeToDelete.next == null) {
+            return false; // Failure (Invalid)
         }
 
         nodeToDelete.data = nodeToDelete.next.data;
         nodeToDelete.next = nodeToDelete.next.next;
 
-        return nodeToDelete;
+        return true;
     }
 
     public static void main() {
         LLNode head = new LLNode(1);
         head.appendToTail(2);
         head.appendToTail(3);
-        delete(head.next); // Delete 2
+        tryDelete(head.next); // Delete 2
 
         while (head != null) {
-            System.out.println(head.data);
+            out.println(head.data);
             head = head.next;
         }
+
+        out.println(tryDelete(new LLNode(100))); // False
     }
 }
