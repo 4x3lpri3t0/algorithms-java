d7e0bfd5daf310949063b4222f1077400f9fa870
Axel Prieto <prietoaxel@gmail.com>
2018-07-19 20:05:27 +0200

    Daily programming practice

--- CtCI/src/_Main.java
+++ CtCI/src/_Main.java
@@ -6,5 +6,6 @@ public class _Main {
 //        _2_1_RemoveDuplicates.main();
 //        _2_2_FindKthToLast.main();
 //        _2_3_DeleteMiddleNode.main();
+//        new _3_2_StackWithMin().main();
     }
 }
