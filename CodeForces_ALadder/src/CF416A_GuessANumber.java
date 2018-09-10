128126de914a457be44b1720929048a3b852ee2d
Axel Prieto <prietoaxel@gmail.com>
2018-09-10 19:48:56 +0200

    minor changes

---
+++ CodeForces_ALadder/src/CF416A_GuessANumber.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class CF416A_GuessANumber extends PrintWriter {
+    public void solve() throws IOException {
+        int n = readInt();
+        int hi =  2 * 1000 * 1000 * 1000;
+        int lo = -hi;
+        for (int i = 0; i < n; i++) {
+            String op = read();
+            Integer x = readInt();
+            String yn = read();
+
+            if(op.equals(">") && yn.equals("Y") || op.equals("<=") && yn.equals("N"))
+                lo = max(lo, x + 1);
+            if(op.equals(">=") && yn.equals("Y") || op.equals("<") && yn.equals("N"))
+                lo = max(lo, x);
+            if(op.equals("<") && yn.equals("Y") || op.equals(">=") && yn.equals("N"))
+                hi = min(hi, x - 1);
+            if(op.equals("<=") && yn.equals("Y") || op.equals(">") && yn.equals("N"))
+                hi = min(hi, x);
+        }
+
+        println(lo <= hi ? lo : "Impossible");
+    }
+
+    BufferedReader in;
+    StringTokenizer stok;
+
+    public void run() {
+        try {
+            solve();
+            close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(abs(-1));
+        }
+    }
+
+    CF416A_GuessANumber() throws IOException {
+        super(System.out);
+        in = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            Locale.setDefault(Locale.US);
+        } catch (Exception ignored) {
+        }
+        new CF416A_GuessANumber().run();
+    }
+
+    String read() throws IOException {
+        while (stok == null || !stok.hasMoreTokens()) {
+            String s = in.readLine();
+            if (s == null) {
+                return null;
+            }
+            stok = new StringTokenizer(s);
+        }
+        return stok.nextToken();
+    }
+
+    int readInt() throws IOException {
+        return Integer.parseInt(read());
+    }
+
+    long readLong() throws IOException {
+        return Long.parseLong(read());
+    }
+
+    double readDouble() throws IOException {
+        return Double.parseDouble(read());
+    }
+
+    int[] readIntArray(int len) throws IOException {
+        int[] a = new int[len];
+        for (int i = 0; i < len; i++) {
+            a[i] = readInt();
+        }
+        return a;
+    }
+
+    <T> List<T>[] createAdjacencyList(int countVertex) {
+        List<T>[] res = new List[countVertex];
+        for (int i = 0; i < countVertex; i++) {
+            res[i] = new ArrayList<T>();
+        }
+        return res;
+    }
+}
