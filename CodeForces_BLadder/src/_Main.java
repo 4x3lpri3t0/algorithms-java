4e0f9299667a9757c0189bd21529997bc4d97220
Axel Prieto <prietoaxel@gmail.com>
2018-12-28 18:49:08 +0100

    Daily programming practice

---
+++ CodeForces_BLadder/src/_Main.java
@@ -0,0 +1,8 @@
+public class _Main {
+
+    public static void main(String[] args) {
+        // TODO: Problems that I still struggle with
+        // TODO: and need to keep reviewing periodically
+        // TODO: (Remove from list after 100% sure I've mastered it)
+    }
+}
