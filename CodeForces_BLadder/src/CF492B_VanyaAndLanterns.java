d80ff4cc3eea79aadfb1cb95a5a24a98e15e70a7
Axel Prieto <prietoaxel@gmail.com>
2019-01-06 21:15:01 +0100

    Daily programming practice

---
+++ CodeForces_BLadder/src/CF492B_VanyaAndLanterns.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class CF492B_VanyaAndLanterns extends PrintWriter {
+    public void solve() throws IOException {
+        int n = readInt();
+        int l = readInt();
+
+        int[] a = readIntArray(n);
+        Arrays.sort(a);
+
+        // Min light radius (start from zero to first lamp)
+        double d = a[0];
+
+        for (int i = 1; i < n; i++) {
+            d = min(d, (a[i] - a[i - 1]) / 2);
+        }
+
+        println(d);
+    }
+
+    BufferedReader in;
+    StringTokenizer stok;
+
+    public void run() {
+        try {
+            solve();
+            close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(abs(-1));
+        }
+    }
+
+    CF492B_VanyaAndLanterns() {
+        super(System.out);
+        in = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public static void main(String[] args) {
+        try {
+            Locale.setDefault(Locale.US);
+        } catch (Exception ignored) {
+        }
+        new CF492B_VanyaAndLanterns().run();
+    }
+
+    String read() throws IOException {
+        while (stok == null || !stok.hasMoreTokens()) {
+            String s = in.readLine();
+            if (s == null) {
+                return null;
+            }
+            stok = new StringTokenizer(s);
+        }
+        return stok.nextToken();
+    }
+
+    int readInt() throws IOException {
+        return Integer.parseInt(read());
+    }
+
+    long readLong() throws IOException {
+        return Long.parseLong(read());
+    }
+
+    double readDouble() throws IOException {
+        return Double.parseDouble(read());
+    }
+
+    int[] readIntArray(int len) throws IOException {
+        int[] a = new int[len];
+        for (int i = 0; i < len; i++) {
+            a[i] = readInt();
+        }
+        return a;
+    }
+
+    <T> List<T>[] createAdjacencyList(int countVertex) {
+        List<T>[] res = new List[countVertex];
+        for (int i = 0; i < countVertex; i++) {
+            res[i] = new ArrayList<T>();
+        }
+        return res;
+    }
+}
